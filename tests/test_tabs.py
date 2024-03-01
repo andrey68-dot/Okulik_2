157db6193ab2b399838dd52ce891de200bb85e40
Andrey <andrey.chudinov68andrei@yandex.ru>
2024-03-01 10:36:50 +0500

    new

--- tests/test_tabs.py
+++ tests/test_tabs.py
@@ -1,7 +1,10 @@
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 
 def test_tabs(browser):
     browser.get('https://magento.softwaretestingboard.com/what-is-new.html')
+    browser.implicitly_wait(10)
     women_button = browser.find_element(By.ID, 'ui-id-4').click()
     men_button = browser.find_element(By.ID, 'ui-id-5').click()
     gear_button = browser.find_element(By.ID, 'ui-id-6').click()
